6a25f97368625c95782314a7b6f3595c6c8355a6
ericheng <eric.heng@supplycart.my>
2020-12-17 11:17:58 +0800

    ST-5969-FIX add service product item api

---
+++ src/Data/Item/Item.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Supplycart\Xero\Data\Item;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class Item extends DataTransferObject
+{
+    public $ItemId;
+
+    public $Code;
+
+    public $Name;
+
+    public $IsSold;
+
+    public $IsPurchased;
+
+    public $Description;
+
+    public $PurchaseDescription;
+
+    public $PurchaseDetails;
+
+    public $SalesDetails;
+
+    public $IsTrackedAsInventory;
+
+    public $InventoryAssetAccountCode;
+
+    public $TotalCostPool;
+
+    public $QuantityOnHand;
+
+    public $UpdatedDateUTC;
+
+    public $HasValidationErrors;
+}
